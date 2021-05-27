dad7fae8cb351aad6815692675dfb7a83832ed17
Juan Sanchez <juan.sanchez@phila.gov>
2021-05-27 07:21:17 -0400

    list items component for dashboard

---
+++ client/src/components/listItems.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import PeopleIcon from '@material-ui/icons/People';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import LayersIcon from '@material-ui/icons/Layers';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import WcIcon from '@material-ui/icons/Wc';
+
+import { Link } from 'react-router-dom';
+
+export const mainListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <WcIcon />
+      </ListItemIcon>
+      <ListItemText primary="Parents" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Students" />
+    </ListItem>
+    <ListItem button component={Link} to="/rewards">
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Rewards" />
+    </ListItem>
+    <ListItem button component={Link} to="/passwordreset">
+      <ListItemIcon>
+        <LayersIcon />
+      </ListItemIcon>
+      <ListItemText primary="Password Reset" />
+    </ListItem>
+  </div>
+);
+
+export const secondaryListItems = (
+  <div>
+    <ListSubheader inset>Classroom List</ListSubheader>
+    <ListItem button>
+      <ListItemIcon>
+        <MeetingRoomIcon />
+      </ListItemIcon>
+      <ListItemText primary="Kindergarden" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <MeetingRoomIcon />
+      </ListItemIcon>
+      <ListItemText primary="1st Grade" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <MeetingRoomIcon />
+      </ListItemIcon>
+      <ListItemText primary="2nd Grade" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <MeetingRoomIcon />
+      </ListItemIcon>
+      <ListItemText primary="3rd Grade" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <MeetingRoomIcon />
+      </ListItemIcon>
+      <ListItemText primary="4th Grade" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <MeetingRoomIcon />
+      </ListItemIcon>
+      <ListItemText primary="5th Grade" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <MeetingRoomIcon />
+      </ListItemIcon>
+      <ListItemText primary="6th Grade" />
+    </ListItem>
+  </div>
+);
