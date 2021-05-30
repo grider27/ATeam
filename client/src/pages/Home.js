b8eafc502b8dfe24c6a26da7967ffed4eefde4e1
Mike Marino <marino1820@gmail.com>
2021-05-30 15:36:40 -0400

    Home page loading html

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,9 @@
 
 const Home = () => {
   return  ( 
-  <div className="container">
+  <div>
+      
+      <div className="container">
 
  
   
@@ -10,26 +12,37 @@ const Home = () => {
     
       <section className="landing">
         
-          <div className="row"> 
-              <div className="col-12" style="height: 2vh;"></div>
-          </div>
+        <div className="row"> 
+        <div className="col-12" style={{height: '2vh'}}></div>
+        </div>
 
-          <div className="row shadow shadow-lg" style=" 
-          background-image: url(assets/imgs/composition-bg.png); 
-          border-top-right-radius: 50px;
-          border-bottom-right-radius: 50px;
-          ">
-              <div className="col-2" style="height:90vh; border-left-style: solid;
-          border-left-width: 80px;">
+                <div id="book" className = "row shadow shadow-lg"
+                style = {{
+                        borderTopRightRadius: '50px',
+                        borderBottomRightRadius: '50px'
+                        }}>
+         
+                      <div className="col-2" style={{
+                          height: '90vh',
+                          borderLeftStyle: 'solid',
+                          borderLeftWidth: '80px'
+              }}>
 
               </div>
               <div className="col-8">
                   <div className="row">
-                      <div className="col-12 d-flex justify-content-end" style="height:10vh; z-index: 1">
+                              <div className="col-12 d-flex justify-content-end" style={{
+                                  height: '10vh',
+                                  zIndex: '1'
+                              }}>
                           <a href="signup.html">
                               <div className="shape-outer heptagon">
                                   <div className="shape-inner heptagon">
-                                      <h4 style="margin-top: 35px; color:red;">ENROLL!</h4>
+                                              <h4 style=
+                                                  {{
+                                                  marginTop: '35px',
+                                                  color:'red'
+                                                  }}>ENROLL!</h4>
                                   </div>
                               </div>
                           </a>
@@ -38,11 +51,13 @@ const Home = () => {
                   </div>
                   <div className="row">
                       <div className="col-8 offset-2 ">
-                          <div className="login" style="
-                              background: white; border-radius: 100px;
-                              border:solid;
-                              border-width: 5px;
-                              ">
+                                  <div className="login" style={{
+                                     background: 'white',
+                                     borderRadius: '100px',
+                                     border: 'solid',
+                                     borderWidth: '5px'
+                                      
+                          }}>
                               <div className="row">
                                   <div className="col-12">
 
@@ -53,7 +68,12 @@ const Home = () => {
                                   <div className="col-12 d-flex justify-content-center">
                                       <input type="username" className="form-control mx-5" id="code"
                                           placeholder="Student Code"
-                                          style=" border-style: none; border-bottom-width: 2px; border-bottom: solid black; z-index: 1;">
+                                                  style={{
+                                                      borderStyle: 'none',
+                                                      borderBottomWidth: '2px',
+                                                      borderBottom: 'solid',
+                                                      zIndex: '1'
+                                          }}></input>
                                   </div>
                               </div>
                               <br />
@@ -61,7 +81,12 @@ const Home = () => {
                                   <div className="col-12 d-flex justify-content-center">
                                       <input type="username" className="form-control mx-5" id="email"
                                           placeholder="Administrator Email"
-                                          style=" border-style: none; border-bottom-width: 2px; border-bottom: solid black; z-index: 1;">
+                                          style={{
+                                                      borderStyle: 'none',
+                                                      borderBottomWidth: '2px',
+                                                      borderBottom: 'solid',
+                                                      zIndex: '1'
+                                          }}></input>
                                   </div>
                               </div>
                               
@@ -70,7 +95,12 @@ const Home = () => {
                                   <div className="col-12 d-flex justify-content-center">
                                       <input type="password" className="form-control mx-5" id="password"
                                           placeholder="Password"
-                                          style="border-style: none; border-bottom-width: 2px; border-bottom: solid black">
+                                          style={{
+                                                      borderStyle: 'none',
+                                                      borderBottomWidth: '2px',
+                                                      borderBottom: 'solid',
+                                                      zIndex: '1'
+                                          }}></input>
                                   </div>
                               </div>
                               <br />
@@ -108,30 +138,25 @@ const Home = () => {
       </section>
 
   </div>
+          </div>
+          
+ <div class="custom-shape-divider-top-1621465308">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M0,0V6c0,21.6,291,111.46,741,110.26,445.39,3.6,459-88.3,459-110.26V0Z" class="shape-fill ">
+                  </path>                  
+                  </svg>
+    </div>
+
+    <div class="custom-shape-divider-bottom-1621466425 ">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path
+                d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+                class="shape-fill"></path>
+        </svg>
+    </div>
+
+
 </div>
-
-
-
-<div className="custom-shape-divider-top-1621465308">
-  <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-      <path d="M0,0V6c0,21.6,291,111.46,741,110.26,445.39,3.6,459-88.3,459-110.26V0Z" className="shape-fill ">
-      </path>
-</div>
-
-<div className="custom-shape-divider-bottom-1621466425 ">
-  <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-      <path
-          d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-          className="shape-fill"></path>
-  </svg>
-</div>;
-
-
-
-
-
-
-
 )}
 
 
