fc937c4330de9e8b0b70463333d4be8033d5b932
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-05-30 16:08:50 -0400

    updated signup.js taken codes from signup.html to render in react

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,5 +1,79 @@
-import React from 'react';
 
-export default function Signup() {
-  return <h2>Signup</h2>;
+const Signup = () => {
+  return (
+    <div className="wrapper">
+    
+    <div className="content">
+
+        <nav className="navbar navbar-expand-lg">
+
+            <a className="navbar-brand" href="index5.html">Reader-Feeder</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            
+        </nav>
+
+        <main className="container block">
+
+            <section className="row">
+
+                <div className="col-md-12">
+
+                    <form className="block form-horizontal">
+
+                        <h2 className="block-header">Create a new Reader-Feeder Program Administrator Account</h2>
+                        <hr />
+
+                        <div className="form-group">
+
+                            <label htmlFor="name" className="col-sm-12 control-label">Name </label>
+
+                            <div className="col-sm-12">
+                                <input type="name" className="form-control" id="name" placeholder="Name" />
+                            </div>
+
+                        </div>
+
+                        <div className="form-group">
+
+                            <label for="email" className="col-sm-12 control-label">Email</label>
+
+                            <div className="col-sm-12">
+                                <input type="email" className="form-control" id="email" placeholder="Email" />
+                            </div>
+
+                        </div>
+
+                        <div className="form-group">
+
+                            <label for="school" className="col-sm-12 control-label">School</label>
+
+                            <div className="col-sm-12">
+                                <input type="school" className="form-control" id="school" placeholder="School Name" />
+                            </div>
+
+                        </div>
+
+                    </form>
+
+                    <a className="nav-link" href="dashboard1.html">
+                        <button className="submit-btn">Create Account</button>
+                    </a>
+
+                </div>
+
+            </section>
+
+        </main>
+
+
+    </div>
+    
+</div>
+
+  );
 }
+
+export default Signup
