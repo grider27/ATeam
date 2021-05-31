3741ce059af346a531387f0079c4ea7a2ffbd470
Vinh Huynh <vinhkhamhuynh@gmail.com>
2021-05-31 19:31:40 -0400

    linked manage students to navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -11,13 +11,13 @@ function NavBar(props) {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="dashboard1.html">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="/TeacherDashboard">Teacher Dashboard <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="manage1.html">Add/Manage Student</a>
+                        <a className="nav-link" href="/ManageStudents">Add/Manage Student</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="index5.html">Sign Out</a>
+                        <a className="nav-link" href="/home">Sign Out</a>
                     </li>
                 </ul>
             </div>
