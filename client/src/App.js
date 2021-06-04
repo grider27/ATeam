4ec6a6a1c217b095658a90dcd567a7abdc31f71c
Juan Sanchez <juan.sanchez@phila.gov>
2021-06-04 11:10:38 -0400

    moved routes to the PrivateRoute file. moved css to this file. added auth token.

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,41 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Dashboard from './pages/Dashboard';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Rewards from './pages/Rewards';
-import PasswordReset from './pages/PasswordReset';
-import ManageStudents from './pages/ManageStudents';
-import TeacherDashboard from './pages/TeacherDashboard';
-import Menu from './pages/Menu';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import Footer from './components/Footer';
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/passwordreset">
-            <PasswordReset />
-          </Route>
-          <Route path="/rewards">
-            <Rewards />
-          </Route>
-          <Route path="/managestudents">
-            <ManageStudents />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/teacherdashboard">
-            <TeacherDashboard />
-          </Route>
-          <Route path="/menu">
-            <Menu />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
-  );
+import './App.css';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
 }
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+          <Footer />
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+export default App;
