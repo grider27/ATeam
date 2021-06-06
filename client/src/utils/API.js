14cdea097daafb8f54c0361bc504019c414e514b
Juan Sanchez <juan.sanchez@phila.gov>
2021-06-06 09:30:51 -0400

    added eslint rule for anonymous export

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,20 @@
-import axios from "axios";
+/* eslint-disable import/no-anonymous-default-export */
+import axios from 'axios';
 
 export default {
+  getStudents: function () {
+    return axios.get('/api/students');
+  },
 
-    getStudents: function () {
-        return axios.get("/api/students");
-    },
-
-    getStudent: function (id) {
-        return axios.get("/api/students/" + id);
-    },
-    // Deletes the book with the given id
-    deleteStudent: function (id) {
-        return axios.delete("/api/students/" + id);
-    },
-    // Saves a book to the database
-    saveStudents: function (studentData) {
-        return axios.post("/api/students", studentData);
-    }
+  getStudent: function (id) {
+    return axios.get('/api/students/' + id);
+  },
+  // Deletes the book with the given id
+  deleteStudent: function (id) {
+    return axios.delete('/api/students/' + id);
+  },
+  // Saves a book to the database
+  saveStudents: function (studentData) {
+    return axios.post('/api/students', studentData);
+  },
 };
