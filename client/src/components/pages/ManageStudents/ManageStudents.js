c1f97f7658b2858b4124283d74eec42c4f965d02
Juan Sanchez <juan.sanchez@phila.gov>
2021-06-06 21:13:47 -0400

    removed comments. updated table. removed unused imports.

--- client/src/components/pages/ManageStudents/ManageStudents.js
+++ client/src/components/pages/ManageStudents/ManageStudents.js
@@ -7,9 +7,7 @@ import { connect } from 'react-redux';
 import Spinner from '../../layout/Spinner';
 import { getCurrentProfile } from '../../../actions/profile';
 import API from '../../../utils/API';
-import { List, ListItem } from '../../../components/List';
 import DeleteBtn from '../../../components/DeleteBtn';
-import { Link } from 'react-router-dom';
 import {
   Table,
   TableBody,
@@ -94,7 +92,6 @@ const ManageStudents = ({
                 </div>
               </div>
             </section>
-
             <section className="bg-light border-0 p-5 m-3">
               <div className="row">
                 <div className="col-12">
@@ -140,31 +137,20 @@ const ManageStudents = ({
                 </div>
               </div>
               {students.length ? (
-                // <List>
-                //   {students.map((student) => (
-                //     <ListItem key={student._id}>
-                //       <Link to={'/students/' + student._id}>
-                //         <strong>
-                //           Name: {student.name} &emsp; Email: {student.email}
-                //         </strong>
-                //       </Link>
-                //       <DeleteBtn onClick={() => deleteStudent(student._id)} />
-                //     </ListItem>
-                //   ))}
-                // </List>
                 <Table size="small">
                   <TableHead>
                     <TableRow>
                       <TableCell>Student Name</TableCell>
                       <TableCell>Email</TableCell>
+                      <TableCell>Books Completed</TableCell>
                     </TableRow>
                   </TableHead>
                   <TableBody>
                     {students.map((student) => (
                       <TableRow key={student._id}>
-                        <TableCell>Name: {student.name}</TableCell>
-                        <TableCell>Email: {student.email}</TableCell>
-                        <TableCell>Books Completed: {student.stars}</TableCell>
+                        <TableCell>{student.name}</TableCell>
+                        <TableCell>{student.email}</TableCell>
+                        <TableCell>{student.stars}</TableCell>
                         <DeleteBtn onClick={() => deleteStudent(student._id)} />
                       </TableRow>
                     ))}
@@ -191,139 +177,5 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
   profile: state.profile,
 });
-
-//end vkh
-
-// const ManageStudents = () => {
-//   return (
-//     <div className="wrapper">
-//       <div className="content">
-//         <main className="container block">
-//           <section className="bg-light border-0 p-5 m-3">
-//             <div className="row">
-//               <div className="col-12">
-//               <h1 className="font-weight-bold">TEACHER HOMEROOM <Edit color="primary"/></h1>
-//               </div>
-//             </div>
-//             <div className="row">
-//               <div className="col-12">
-//               School Name
-//               </div>
-//             </div>
-//             <div className="row">
-//               <div className="col-12">
-//               1234 Main Street
-//               </div>
-//             </div>
-//             <div className="row">
-//               <div className="col-12">
-//               Philadelphia, PA 19103
-//               </div>
-//             </div>
-//             </section>
-
-//             <section className="bg-light border-0 p-5 m-3">
-//             <div className="row">
-//               <div className="col-12">
-//                 <h2 className="font-weight-bold">Add Students</h2>
-//               </div>
-//             </div>
-//             <div className="row">
-//               <div className="col-4">
-//                 <input
-//                   type="name"
-//                   className="form-control"
-//                   id="name"
-//                   placeholder="Name"
-//                 />
-//               </div>
-//               <div className="col-4">
-//                 <input
-//                   type="email"
-//                   className="form-control"
-//                   id="email"
-//                   placeholder="Email"
-//                 />
-//               </div>
-//               <div className="col-4">
-//                 <button type="button" className="btn-primary btn-sm">
-//                   Add Student
-//                 </button>
-//               </div>
-//             </div>
-//           </section>
-//           <section className="bg-light border-0 p-5 m-3">
-//           <div className="row">
-//               <div className="col-12">
-//                 <h2 className="font-weight-bold">Classroom</h2>
-//               </div>
-//             </div>
-//             <div className="row">
-//               <div className="col-5">
-//               <h4>Bart Simpson</h4>
-//               </div>
-//               <div className="col-5">
-//               <h4>homer@gmail.com</h4>
-//               </div>
-//               <div className=" col-2 d-flex justify-content-end">
-//                 <Edit color="primary"/>
-//                 <Delete color='secondary'/>
-//               </div>
-//             </div>
-//             <div className="row">
-//               <div className="col-5">
-//               <h4>Nelson Muntz</h4>
-//               </div>
-//               <div className="col-5">
-//               <h4>nelson@gmail.com</h4>
-//               </div>
-//               <div className=" col-2 d-flex justify-content-end">
-//                 <Edit color="primary"/>
-//                 <Delete color='secondary'/>
-//               </div>
-//             </div>
-//             <div className="row">
-//               <div className="col-5">
-//               <h4>Lisa Simpson</h4>
-//               </div>
-//               <div className="col-5">
-//               <h4>homer@gmail.com</h4>
-//               </div>
-//               <div className=" col-2 d-flex justify-content-end">
-//                 <Edit color="primary"/>
-//                 <Delete color='secondary'/>
-//               </div>
-//             </div>
-//             <div className="row">
-//               <div className="col-5">
-//               <h4>Martin Prince</h4>
-//               </div>
-//               <div className="col-5">
-//               <h4>Prince@gmail.com</h4>
-//               </div>
-//               <div className=" col-2 d-flex justify-content-end">
-//                 <Edit color="primary"/>
-//                 <Delete color='secondary'/>
-//               </div>
-//             </div>
-//             <div className="row">
-//               <div className="col-5">
-//               <h4>Ralph Wiggum</h4>
-//               </div>
-//               <div className="col-5">
-//               <h4>wigout@gmail.com</h4>
-//               </div>
-//               <div className=" col-2 d-flex justify-content-end">
-//                 <Edit color="primary"/>
-//                 <Delete color='secondary'/>
-//               </div>
-//             </div>
-
-//             </section>
-//         </main>
-//       </div>
-//     </div>
-//   );
-// };
 
 export default connect(mapStateToProps, { getCurrentProfile })(ManageStudents);
